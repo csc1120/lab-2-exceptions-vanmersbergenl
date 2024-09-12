0312bd42fbb574ebce645c2acbc0ae324b6d69be
vanmersbergenl <vanmersbergenl@msoe.edu>
2024-09-12 11:25:53 -0500

    Die class and DieNotRolledException completed, commit 1

---
+++ src/vanmersbergenl/Driver.java
@@ -0,0 +1,16 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * Main Driver class
+ * Name: Landon Van Mersbergen
+ * Last Updated: 9/12/2024
+ */
+package vanmersbergenl;
+
+/**
+ * The main class
+ */
+public class Driver {
+    public static void main(String[] args) {
+    }
+}
